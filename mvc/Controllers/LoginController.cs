674858944b46202b8781b91bc30a0c8f7fb818fd
athirakrishnan18 <109476459+athirakrishnan18@users.noreply.github.com>
2022-07-27 14:23:43 +0530

    Add files via upload

--- mvc/Controllers/LoginController.cs
+++ mvc/Controllers/LoginController.cs
@@ -3,25 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HospitalManagementNew.Repository;
 using HospitalManagementNew.Models;
+using System.Data;
+using HospitalManagementNew.Repository;
+using System.Web.Security;
+
 
 
 namespace HospitalManagementNew.Controllers
 {
+   
     public class LoginController : Controller
     {
+
+        LoginRepository loginrepository = new LoginRepository();
         // GET: Login
         public ActionResult Login()
         {
-            
+
             return View();
+        }
+        [HttpPost]
+        
+        public ActionResult Login(Login login)  //role based login check
+        {
+            try
+            {
+                var roles = loginrepository.Logincheck(login);
+                if (roles[0].role.ToString() != "user")
+                {
+                    TempData["success"] = "Login successfully";
+                    return RedirectToAction("Index","patient");     //redirecting to patiemt view page
+                }
+                else
+                {
+                    TempData["error"] = "Login failed";
+                }
+                //return RedirectToAction("Index", patientmodel);
+                return View(login);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
-        public ActionResult LoginUser()
-        {
-            return View();
-        }
-      
+
+
+
     }
+
+
+
 }
